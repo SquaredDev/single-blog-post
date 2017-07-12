261bad22e684ff713bb95484806e6af257f1162a
James <boydston.j@gmail.com>
2017-07-12 10:12:57 -0700

    starting

--- main.js
+++ main.js
@@ -13,4 +13,27 @@ const blogPost = {
   }
 };
 
+
+// <article>
+//   <h2>The Amazing Sloth</h2>
+//   <div class="meta">
+//     <span class="date">Written on May 2, 2017</span>
+//   </div>
+//   <div class="post">
+//     Sloths make very good habitats for other organisms, and a single sloth may be home to several species of moths, beetles, cockroaches, fungi, ciliates, and algae.*
+//   </div>
+//   <div class="site">
+//     Part of this content was pulled from <a href="https://en.wikipedia.org/wiki/Sloth">Wikipedia</a>
+//   </div>
+// </article>
+
 // Start Editing Here
+
+let htmlString = "<article>"
+
+blogPost.forEach(function(item) {
+
+}
+
+
+htmlString = htmlString + '</article>'
